27a0aae1906c9558db84457e50bd5d9d2a57ecba
maxpichet <60222646+crackmax@users.noreply.github.com>
2020-09-17 17:51:45 -0400

    exercise fini

--- exercice.py
+++ exercice.py
@@ -4,23 +4,39 @@
 import random
 
 def is_even_len(string):
-	return 0xDEADBEEF
+
+	return len(string) % 2 == 0
+
 
 
 def get_num_char(string, char):
-	return 0xDEADBABE
+	occurence = 0
+	for x in string:
+		if x == char:
+			occurence +=1
+
+	return occurence
+
+
+
 
 
 def get_first_part_of_name(name):
-	return ""
+	split_name =  name.split('-')[0]
+	cap = split_name[0].upper() + split_name[1:].lower()
+	return "Bonjour, " + cap
 
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	basic_sentence = "Aujourd’hui, j’ai vu un %s s’emparer d’un panier %s plein de %s ."
 
+	words = []
+	for word_set in (animals, adjectives, fruits):
+		words += [word_set[random.randrange(0, len(word_set))]]
+	return basic_sentence % tuple(words)
 
 if __name__ == "__main__":
-	spam = "Bonjour!"
+	spam = "Bonjour"
 	parity = "pair" if is_even_len(spam) else "impair"
 	print(f"Le nombre de caractère dans la chaine '{spam}' est {parity}.")
 
